5457ab61d21d1b7af116d61f5340bae4a069ae3c
Airjam85 <ogjarrett@gmail.com>
2026-05-19 00:04:50 -0400

    adding back templateaction2s

--- app/[category]/[slug]/page.js
+++ app/[category]/[slug]/page.js
@@ -1,7 +1,7 @@
 import templates from "../../../content/templates.json";
 import Link from "next/link";
 import { notFound } from "next/navigation";
-import TemplateActions from "../../../../components/TemplateActions";
+import TemplateActions from "../../../components/TemplateActions";
 
 export async function generateStaticParams() {
   return templates.map((template) => ({
