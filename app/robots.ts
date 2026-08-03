3f8df9788f4e7bbfd39998d1d7610e880dd1af4b
Airjam85 <ogjarrett@gmail.com>
2026-08-03 15:47:00 -0400

    Fix robots site URL

--- app/robots.ts
+++ app/robots.ts
@@ -1,7 +1,8 @@
 import type { MetadataRoute } from "next";
+import { SITE_URL } from "../lib/constants";
 
 export default function robots(): MetadataRoute.Robots {
-  const baseUrl = "https://template-site-pink.vercel.app";
+  const baseUrl = SITE_URL;
 
   return {
     rules: [
