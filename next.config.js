8ae619e9aa9586e19e3297ccd2629c418732a295
Airjam85 <ogjarrett@gmail.com>
2026-08-03 16:04:17 -0400

    Map legacy template redirects

--- next.config.js
+++ next.config.js
@@ -1,7 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   async redirects() {
+    const { templates } = await import("./lib/loadTemplates.js");
+
     return [
+      ...templates.map((template) => ({
+        source: `/templates/${template.slug}`,
+        destination: `/${template.category.slug}/${template.slug}`,
+        permanent: true,
+      })),
       {
         source: "/templates/:slug",
         destination: "/",
